5ee8b6c08134148223f05fd3ad7678ae3e1204b6
kimheekimhee <khd128k@gmail.com>
2022-09-22 17:52:43 +0900

    django

---
+++ django/0922/django_0922/training/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+# HTTP : 요청(request)을 하면 무언가를 응답(response)하는 방식
+# index 함수 선언  정의
+def index(request):
+
+    return render(request, "index.html")
+
+def template(request):
+    _number = 1
+    context = {
+        "number" : _number
+    }
+    return render(request, "template.html", context)
+
+def dinner(request):
+
+    return render(request, "dinner.html")
