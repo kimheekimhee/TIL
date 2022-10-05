d4fdd60d23bc2217fddfc7f1bc7f7475a12bdef9
kimheekimhee <khd128k@gmail.com>
2022-10-05 16:18:20 +0900

    alogorithm

---
+++ python/BOJ/BOJ_8958.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.stdin = open("BOJ_8958.txt", "r")
+
+n = int(input())
+
+for _ in range(n):
+    ox_list = list(input())
+    score = 0
+    sum_score = 0
+    for ox in ox_list:
+        if ox == "O":
+            score += 1
+            sum_score += score
+        else:
+            score = 0
+    print(sum_score)
