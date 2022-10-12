99db23ca991c3665abce7cc44d33e08138db235d
kimheekimhee <khd128k@gmail.com>
2022-10-12 18:36:28 +0900

    django

--- django/1012/articles/admin.py
+++ django/1012/articles/admin.py
@@ -1,3 +1,15 @@
+# articles/admin.py 을 아래와 같이 변경
+
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at", "updated_at")
+
+
+admin.site.register(Article, ArticleAdmin)
+
+# 여기까지 작성하고 http://127.0.0.1:8000/admin/ 에 로그인해보면
+# Article objects 라고 표시되던 컨텐츠들이
+# 전부 각각의 제목, 작성시간, 수정시간으로 표시되고 있음을 확인 가능
