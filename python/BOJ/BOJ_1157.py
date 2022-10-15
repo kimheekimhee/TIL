32fead91fcb7c2c56416dff1cb0f0b8f16bbd524
unknown <khd128k@gmail.com>
2022-10-15 20:58:49 +0900

    algorithm

--- python/BOJ/BOJ_1157.py
+++ python/BOJ/BOJ_1157.py
@@ -5,11 +5,12 @@ sys.stdin = open("BOJ_1157.txt", "r")
 word = input().upper()
 unique = list(set(word))
 cnt = []
-
+# print(word, unique) << MISSISSIPI ['S', 'P', 'M', 'I']
 for i in unique:
     count_word = word.count(i)
     cnt.append(count_word)
-
+print(cnt)
+print(cnt.count(count_word))
 if cnt.count(max(cnt)) > 1:
     print("?")
 else:
