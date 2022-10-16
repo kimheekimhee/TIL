2dfa112a800b6cc9e4be591c7451a0dee256de24
kimheekimhee <khd128k@gmail.com>
2022-10-16 15:47:56 +0900

    algorithm

--- python/BOJ/BOJ_1302.py
+++ python/BOJ/BOJ_1302.py
@@ -1,23 +1,3 @@
 import sys
 
 sys.stdin = open("BOJ_1302.txt", "r")
-
-n = int(input())
-books = {}
-
-for _ in range(n):
-    book = input()
-    if book not in books:
-        books[book] = 1
-    else:
-        books[book] += 1
-
-max_freq = max(books.values())
-
-best_seller = []
-
-for book, number in books.items():
-    if number == max_freq:
-        best_seller.append(book)
-
-print(sorted(best_seller)[0])
---
+++ python/BOJ/BOJ_2579.py
@@ -0,0 +1,21 @@
+import sys
+
+sys.stdin = open("BOJ_2579.txt", "r")
+
+import sys
+
+input = sys.stdin.readline
+arr = []
+dp = []
+
+l = int(input())
+for _ in range(l):
+    arr.append(int(input()))
+
+dp.append(arr[0])
+dp.append(max(arr[0] + arr[1], arr[1]))
+dp.append(max(arr[0] + arr[2], arr[1] + arr[2]))
+for i in range(3, l):
+    dp.append(max(dp[i - 2] + arr[i], dp[i - 3] + arr[i] + arr[i - 1]))
+
+print(dp.pop())
