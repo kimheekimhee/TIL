52a57ca001dd07e69de94e80fd2101d20e5b4a09
kimheekimhee <khd128k@gmail.com>
2022-10-17 15:58:51 +0900

    django

---
+++ django/1017/articles/models.py
@@ -0,0 +1,10 @@
+from email.mime import image
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(blank=True, upload_to="imanges/")
