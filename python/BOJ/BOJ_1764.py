c43a61fa7b1d3cf31f8801d6740c624fdfcf0579
kimheekimhee <khd128k@gmail.com>
2022-12-07 23:10:14 +0900

    algorithm

---
+++ python/BOJ/BOJ_1764.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.stdin = open("BOJ_1764.txt", "r")
+
+
+N , M = map(int,input().split())
+arr_1 = set()
+arr_2 = set()
+
+for _ in range(N):
+    arr_1.add(input())
+for _ in range(M):
+    arr_2.add(input())
+
+arr = sorted(list(arr_1 & arr_2))
+print(len(arr))
+
+for i in arr:
+    print(i)
