84c87848fbb73b124232e60bc7e7c1098b0bb9a0
kimheekimhee <khd128k@gmail.com>
2022-12-27 09:58:34 +0900

    algorithm

---
+++ python/BOJ/BOJ_2525.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('BOJ_2525.txt', 'r')
+
+H, M = map(int, input().split())
+timer = int(input()) 
+
+H += timer // 60
+M += timer % 60
+
+if M >= 60:
+    H += 1
+    M -= 60
+if H >= 24:
+    H -= 24
+
+print(H,M)
