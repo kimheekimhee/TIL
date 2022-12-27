89620113644eb9a63b218f4fcb9f2d7ee4632469
kimheekimhee <khd128k@gmail.com>
2022-12-28 00:18:35 +0900

    algorithm

--- python/BOJ/BOJ_2525.py
+++ python/BOJ/BOJ_2525.py
@@ -1,16 +1,8 @@
-import sys
-sys.stdin = open('BOJ_2525.txt', 'r')
+# import sys
+# sys.stdin = open('BOJ_15596.txt', 'r')
 
-H, M = map(int, input().split())
-timer = int(input()) 
-
-H += timer // 60
-M += timer % 60
-
-if M >= 60:
-    H += 1
-    M -= 60
-if H >= 24:
-    H -= 24
-
-print(H,M)
+def solve(a):
+    total = 0
+    for x in a:
+        total += x
+    return total
