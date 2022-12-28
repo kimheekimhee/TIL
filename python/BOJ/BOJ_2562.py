619acf4e2e4731e30ea533f96fd5a7eec6a6f1a7
kimheekimhee <khd128k@gmail.com>
2022-12-29 00:15:26 +0900

    algorithm

---
+++ python/BOJ/BOJ_2562.py
@@ -0,0 +1,10 @@
+import sys
+
+sys.stdin = open("BOJ_2562.txt", "r")
+
+num_list = []
+for i in range(9):
+    num_list.append(int(input()))
+
+print(max(num_list))
+print(num_list.index(max(num_list))+1)
