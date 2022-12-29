f738b55dd6dab343474be418406d377f922e5347
kimheekimhee <khd128k@gmail.com>
2022-12-30 02:03:35 +0900

    algorithm

---
+++ python/BOJ/BOJ_5597.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.stdin = open("BOJ_5597.txt", "r")
+
+students = [i for i in range(1,31)]
+
+for _ in range(28):
+    applied = int(input())
+    students.remove(applied)
+
+print(min(students))
+print(max(students))
