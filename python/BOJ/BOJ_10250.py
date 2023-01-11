ee1f4f7f5934f00a0c8884460821b83dedb8d3f2
kimheekimhee <khd128k@gmail.com>
2023-01-11 23:10:03 +0900

    algorithm

---
+++ python/BOJ/BOJ_10250.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.stdin = open("BOJ_10250.txt", "r")
+
+t = int(input())
+
+for i in range(t):
+    h, w, n = map(int, input().split())
+    num = n//h + 1
+    floor = n % h
+    if n % h == 0:  # h의 배수이면,
+        num = n//h
+        floor = h
+    print(f'{floor*100+num}')
