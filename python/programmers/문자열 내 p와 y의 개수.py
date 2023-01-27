2e8118e731002b5201ea55680839387dc2bb0690
kimheekimhee <khd128k@gmail.com>
2023-01-27 14:54:49 +0900

    algorithm

---
+++ python/programmers/문자열 내 p와 y의 개수.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = True
+    
+    p = 0
+    y = 0
+    
+    for i in s:
+        if i == 'p' or i =='P':
+            p += 1
+        elif i == 'y' or i == 'Y':
+            y += 1
+    
+    if p != y:
+        answer = False
+    
+    return answer
