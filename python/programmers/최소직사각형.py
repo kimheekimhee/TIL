a153b27c62fe67958f3510611795bf973b0e8df4
kimheekimhee <khd128k@gmail.com>
2023-03-27 22:55:24 +0900

    algorithm

---
+++ python/programmers/최소직사각형.py
@@ -0,0 +1,13 @@
+def solution(sizes):
+    answer = 0
+    w = []
+    h = []
+    for i in range(len(sizes)):
+        if sizes[i][0] >= sizes[i][1]:
+            w.append(sizes[i][0])
+            h.append(sizes[i][1])
+        else:
+            h.append(sizes[i][0])
+            w.append(sizes[i][1])
+    answer += max(w) * max(h)
+    return answer
