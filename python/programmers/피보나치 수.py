f4545658bdfe9880fc48c60e2e14eb31f28822d3
kimheekimhee <khd128k@gmail.com>
2023-06-08 17:59:56 +0900

    algorithm

--- python/programmers/피보나치 수.py
+++ python/programmers/피보나치 수.py
@@ -1,5 +1,10 @@
 def solution(n):
-    a, b = 0, 1
-    for i in range(n):
-        a, b = b, a + b
-    return a
+    answer = []
+    for i in range(n+1):
+        if i==0 or i==1:
+            answer.append(i)
+        else:
+            f = answer[i-1] + answer[i-2]
+            answer.append(f % 1234567)
+
+    return answer[-1]
