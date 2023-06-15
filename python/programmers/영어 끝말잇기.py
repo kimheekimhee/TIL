20d1d45215d0d89830384ed34f5777fc1a664681
kimheekimhee <khd128k@gmail.com>
2023-06-15 23:23:11 +0900

    algorithm

---
+++ python/programmers/영어 끝말잇기.py
@@ -0,0 +1,15 @@
+def solution(n, words):
+    answer = [0,0]
+
+    cnt = 0
+    checks = [words[0]]
+    for i in range(1, len(words)):
+        cnt += 1
+        if words[i] not in checks and list(words[i-1])[-1] == list(words[i])[0]:
+            checks.append(words[i])
+        else:
+            answer[0] = cnt % n + 1
+            answer[1] = cnt // n + 1
+            break
+
+    return answer
