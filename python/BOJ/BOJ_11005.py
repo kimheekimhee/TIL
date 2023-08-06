ad981e156d2328dec0ee97115cb723b212423e13
kimheekimhee <khd128k@gmail.com>
2023-08-07 00:36:12 +0900

    algorithm

---
+++ python/BOJ/BOJ_11005.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('BOJ_11005.txt', 'r')
+
+N, B = map(int, input().split())
+s = ''
+arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+while N:
+    s += str(arr[N%B])
+    N //= B
+
+print(s[::-1])
