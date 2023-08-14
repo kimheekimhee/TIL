f989efb31a22be17e81bd8ed2ab4d7ae0b831e05
kimheekimhee <khd128k@gmail.com>
2023-08-14 23:14:59 +0900

    algorithm

---
+++ python/BOJ/BOJ_9012.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.stdin = open("BOJ_9012.txt", "r")
+
+n = int(input())
+
+for _ in range(n):
+    line = input()
+    stack = []
+
+    for i in line:
+        if i == '(':
+            stack.append('(')
+        elif i == ')':
+            if len(stack) == 0:
+                stack.append(')')
+                break
+            else:
+                stack.pop()
+
+    if len(stack) != 0:
+        print('NO')
+    else:
+        print('YES')
